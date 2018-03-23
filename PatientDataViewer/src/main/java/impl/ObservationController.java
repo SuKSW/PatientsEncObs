87c29ec0864ca95b98cacb1b75a3ec4fbcbabbe7
Subhashinie <subhashinieksw.14@cse.mrt.ac.lk>
2018-03-23 13:51:55 +0530

    Cors support added to PatientDataViewer

--- PatientDataViewer/src/main/java/impl/ObservationController.java
+++ PatientDataViewer/src/main/java/impl/ObservationController.java
@@ -4,11 +4,12 @@ import exceptions.JsonFileReadingException;
 import extra.Config;
 import org.json.simple.JSONObject;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@ComponentScan("impl")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/observations")
 public class ObservationController {
--- PatientDataViewer/src/main/java/impl/PatientController.java
+++ PatientDataViewer/src/main/java/impl/PatientController.java
@@ -5,17 +5,17 @@ import exceptions.JsonFileReadingException;
 import extra.Config;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
-
-    @Autowired
-    PatientController() {
-    }
 
     @RequestMapping(method = RequestMethod.GET)
     JSONObject getAllPatients() throws JsonFileReadingException {
